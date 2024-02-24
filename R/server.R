b27cf4e78ce71736ae352f14be4d24f6c028cb9d
JasperOtieno <jaotie@gmail.com>
2024-02-25 02:36:59 +0300

    updated data path

--- R/server.R
+++ R/server.R
@@ -3,7 +3,7 @@ library(shiny)
 library(tidyverse)
 
 # # Read in data
-adult <- read_csv("./clean_data/adult.csv")
+adult <- read_csv("../clean_data/adult.csv")
 
 # Convert column names to lowercase for convenience 
 names(adult) <- tolower(names(adult))
