64a9f06dd37ed6dbf8676bb213fa1a49bc36664f
JasperOtieno <jaotie@gmail.com>
2024-02-24 04:07:47 +0300

    new update

---
+++ README.md
@@ -0,0 +1,61 @@
+<<<<<<< HEAD
+# RShiny Visualizatiton Project
+
+This is a visualization dashboard project taken to gain knowledge on creating a visualization app using RShiny as part of the R Visualization online course offered by Coursera.
+
+## Objective
+
+**Primary:** To determine the right demographics from the US census data to advertise to for the marketing dpt.
+
+**Secondary:** To build an interactive shiny dashboard to explore the census data for the right demographics.
+
+*The dashboad is aimed at answering the following questions:*
+
+Does education level impact the salary?
+
+Impact of age and sex on salary?
+
+People from which native countries are given less income in spite of having good education?
+
+Does number of hours working contribute to less income?
+
+Which workclass faces less income?
+
+## Folders and Files
+README.md file provides information about the project and its objective.
+
+ui.r and server.r files: program files to create the app.
+
+clean_data folder: folder holding the clean adult data for use in csv format.
+<<<<<<< HEAD
+=======
+
+>>>>>>> jo_update
+rsconnect folder: connection for publishing the app in shinyapps.io
+
+=======
+# RShiny Visualizatiton Project
+
+This is a visualization dashboard project taken to gain knowledge on creating a visualization app using RShiny as part of the R Visualization online course offered by Coursera.
+
+## Objective
+
+Primary: To determine the right demographics from the US census data to advertise to for the marketing dpt.
+
+Secondary: To build an interactive shiny dashboard to explore the census data for the right demographics.
+
+The dashboad is aimed at answering the following questions:
+
+Does education level impact the salary?
+Impact of age and sex on salary?
+People from which native countries are given less income in spite of having good education?
+Does number of hours working contribute to less income?
+Which workclass faces less income?
+
+## Folders and Files
+README.md file provides information about the project and its objective.
+ui.r and server.r files: program files to create the app
+clean_data folder: folder holding the clean adult data for use in csv format
+rsconnect folder: connection for publishing the app in shinyapps.io
+
+>>>>>>> refs/remotes/jo_update/main
---
+++ server.R
@@ -0,0 +1,128 @@
+<<<<<<< HEAD
+# # Load libraries
+library(shiny)
+library(tidyverse)
+
+# # Read in data
+adult <- read_csv("./clean_data/adult.csv")
+
+# Convert column names to lowercase for convenience 
+names(adult) <- tolower(names(adult))
+
+# Define server logic
+
+shinyServer(function(input, output) {
+  
+  df_country <- reactive({
+    adult %>% filter(native_country == input$country)
+  })
+  
+  
+  # TASK 5: Create logic to plot histogram or boxplot
+  output$p1 <- renderPlot({
+    if (input$graph_type == "histogram") {
+      # Histogram
+      ggplot(df_country(), aes_string(x = input$continuous_variable)) +
+        geom_histogram() +  # histogram geom
+        labs(x ="Age",
+             y = "Number of People",
+             title = "Trend of age") +  # labels
+        facet_wrap(~prediction)    # facet by prediction
+    }
+    else {
+      # Boxplot
+      ggplot(df_country(), aes_string(y = input$continuous_variable)) +
+        geom_boxplot() +  # boxplot geom
+        coord_flip() +  # flip coordinates
+        labs(title = "Trend of age",
+             y ="Age" ) +  # labels
+        facet_wrap(~prediction)    # facet by prediction
+    }
+    
+  })
+  
+  # TASK 6: Create logic to plot faceted bar chart or stacked bar chart
+  output$p2 <- renderPlot({
+    # Bar chart
+    p <- ggplot(df_country(), aes_string(x = input$categorical_variable)) +
+      labs(title = "Trend of education",
+           x = "Education",
+           y = "Number of People") +  # labels
+      theme(axis.text.x = element_text(angle =0.45))
+    theme(legend.position = "bottom")  # modify theme to change text angle and legend position
+    
+    if (input$is_stacked) {
+      p + geom_bar(aes(fill = prediction))  # add bar geom and use prediction as fill
+    }
+    else{
+      p + 
+        geom_bar(aes(fill = input$categorical_variable)) + # add bar geom and use input$categorical_variables as fill 
+        facet_wrap(~prediction)   # facet by prediction
+    }
+  })
+  
+})
+=======
+# # Load libraries
+library(shiny)
+library(tidyverse)
+
+# # Read in data
+adult <- read_csv("./clean_data/adult.csv")
+
+# Convert column names to lowercase for convenience 
+names(adult) <- tolower(names(adult))
+
+# Define server logic
+
+shinyServer(function(input, output) {
+  
+  df_country <- reactive({
+    adult %>% filter(native_country == input$country)
+  })
+  
+  # TASK 5: Create logic to plot histogram or boxplot
+  output$p1 <- renderPlot({
+    if (input$graph_type == "histogram") {
+      # Histogram
+      ggplot(df_country(), aes_string(x = input$continuous_variable)) +
+        geom_histogram() +  # histogram geom
+        labs(x ="Age",
+             y = "Number of People",
+             title = "Trend of age") +  # labels
+        facet_wrap(~prediction)    # facet by prediction
+    }
+    else {
+      # Boxplot
+      ggplot(df_country(), aes_string(y = input$continuous_variable)) +
+        geom_boxplot() +  # boxplot geom
+        coord_flip() +  # flip coordinates
+        labs(title = "Trend of age",
+             y ="Age" ) +  # labels
+        facet_wrap(~prediction)    # facet by prediction
+    }
+    
+  })
+  
+  # TASK 6: Create logic to plot faceted bar chart or stacked bar chart
+  output$p2 <- renderPlot({
+    # Bar chart
+    p <- ggplot(df_country(), aes_string(x = input$categorical_variable)) +
+      labs(title = "Trend of education",
+           x = "Education",
+           y = "Number of People") +  # labels
+      theme(axis.text.x = element_text(angle =0.45))
+    theme(legend.position = "bottom")  # modify theme to change text angle and legend position
+    
+    if (input$is_stacked) {
+      p + geom_bar(aes(fill = prediction))  # add bar geom and use prediction as fill
+    }
+    else{
+      p + 
+        geom_bar(aes(fill = input$categorical_variable)) + # add bar geom and use input$categorical_variables as fill 
+        facet_wrap(~prediction)   # facet by prediction
+    }
+  })
+  
+})
+>>>>>>> refs/remotes/jo_update/main
